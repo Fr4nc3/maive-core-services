e0fe0fc07b29f6cb37d1677ef795959123830431
Francia Riesco <friesco+microsoft@microsoft.com>
2026-03-17 15:28:57 -0400

    Add rotate MonoBehaviour and MyScripts asmdef
    Add a new rotate.cs MonoBehaviour that rotates its GameObject around the Y axis each frame using a public rotateSpeed field. Introduce an Assembly Definition (MyScripts.asmdef) referencing SpatialSys.UnitySDK and Unity.TextMeshPro to group the scripts, and include the corresponding Unity .meta files.

---
+++ src/unity/scripts/rotate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+ public float rotateSpeed = 50f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+}
